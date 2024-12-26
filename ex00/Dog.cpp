d91515af1e9c3a53c20f56309187bc6e3bdbf1a6
PiotrJerzy13 <138906583+PiotrJerzy13@users.noreply.github.com>
2024-12-26 09:36:20 +0100

    adding dog constructor

---
+++ ex00/Dog.cpp
@@ -0,0 +1,18 @@
+#include "Dog.hpp"
+
+Dog::Dog() 
+{
+    type = "Dog";
+    std::cout << "Dog Constructor called" << std::endl;
+}
+
+Dog::~Dog() 
+{
+    std::cout << "Dog Destructor called" << std::endl;
+}
+
+void Dog::makeSound() const 
+{
+    std::cout << "Woof! Woof!" << std::endl;
+}
+
