09f6cc1082ce28f3255379122c76749aa0c4b332
PiotrJerzy13 <138906583+PiotrJerzy13@users.noreply.github.com>
2024-12-26 09:39:48 +0100

    adding cat constructor

---
+++ ex00/Cat.cpp
@@ -0,0 +1,17 @@
+#include "Cat.hpp"
+
+Cat::Cat() 
+{
+    type = "Cat";
+    std::cout << "Cat Constructor called" << std::endl;
+}
+
+Cat::~Cat() 
+{
+    std::cout << "Cat Destructor called" << std::endl;
+}
+
+void Cat::makeSound() const
+{
+    std::cout << "Meow! Meow!" << std::endl;
+}
