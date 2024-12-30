fe8598a0252eaaca04aef21cb78dbb251c459788
PiotrJerzy13 <138906583+PiotrJerzy13@users.noreply.github.com>
2024-12-30 15:26:43 +0100

    removing unused functions

--- ex01/Brain.cpp
+++ ex01/Brain.cpp
@@ -31,19 +31,3 @@ Brain::~Brain()
     std::cout << "Brain Destructor called" << std::endl;
 }
 
-void Brain::setIdea(int index, const std::string& idea)
-{
-    if (index >= 0 && index < 100) 
-	{
-        ideas[index] = idea;
-    }
-}
-
-std::string Brain::getIdea(int index) const
-{
-    if (index >= 0 && index < 100) 
-	{
-        return ideas[index];
-    }
-    return "Invalid Index";
-}
